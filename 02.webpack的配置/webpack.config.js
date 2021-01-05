f555ff5e7e713475d1180cbbc2911ffb3277b0d2
ede <837221025@qq.com>
2021-01-06 01:48:49 +0800

    文件配置

--- 02.webpack的配置/webpack.config.js
+++ 02.webpack的配置/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         //css-loader只是负责将CSS文件进行加载  
         //style-loader负责将样式添加到DOM中  
         //使用多个loader时，是从右向左
-        use: ['style-loader','css-loader']
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
