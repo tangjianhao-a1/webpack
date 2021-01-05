4d4d73bf34ffd2cd9649d1ad3ff369525fa501ae
ede <837221025@qq.com>
2021-01-06 01:18:35 +0800

    css样式配置

--- 02.webpack的配置/src/main.js
+++ 02.webpack的配置/src/main.js
@@ -1,12 +1,15 @@
 
 //1.使用commonjs的模块化规范
-const {add,nul} = require('./mathUtils.js')  
+const {add,nul} = require('./js/mathUtils.js')  
 console.log(add(20,30));  
 console.log(nul(20,30));  
 
-//使用es6的模块化规范
-import {name,age,height} from "./info"  
+//2使用es6的模块化规范
+import {name,age,height} from "./js/info";  
 
 console.log(name);
 console.log(age);  
-console.log(height);
+console.log(height);  
+
+//3.依赖css文件  
+require('./css/normal.css')
--- 02.webpack的配置/webpack.config.js
+++ 02.webpack的配置/webpack.config.js
@@ -2,9 +2,21 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/main.js',  
+  entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname,'dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        //css-loader只是负责将CSS文件进行加载  
+        //style-loader负责将样式添加到DOM中  
+        //使用多个loader时，是从右向左
+        use: ['style-loader','css-loader']
+      }
+    ]
+  }
+
 }
