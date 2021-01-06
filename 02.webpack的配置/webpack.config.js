087d7140bea7ce30ff6d5f1facce393dc84d9b84
ede <837221025@qq.com>
2021-01-06 22:18:52 +0800

    webpack处理图片需要下载相关的loader，当处理的图片小于限制时，url-loader能正常加载，当加载的图片大于限制时，需要使用file-loader进行加载，file-loader会把图片新生成到打包文件夹下，此时url路径会出问题，需要在设置出口那里设置出口路径，使用file-loader新生成的图片是由hash值随机生成的，存在命名问题，此时需要在设置限制图片大小那里添加name属性，并自己设置目录和命名格式

--- 02.webpack的配置/webpack.config.js
+++ 02.webpack的配置/webpack.config.js
@@ -3,11 +3,12 @@ const path = require('path')
 
 module.exports = {
   /* 入口 */
-  entry: './src/main.js',  
+  entry: './src/main.js',
   /* 出口 */
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',  
+    publicPath: 'dist/'
   },
   /* 配置css打包 */
   module: {
@@ -17,17 +18,32 @@ module.exports = {
         //css-loader只是负责将CSS文件进行加载  
         //style-loader负责将样式添加到DOM中  
         //使用多个loader时，是从右向左
-        use: ['style-loader',  'css-loader']
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.less$/,  
+        test: /\.less$/,
         use: [{
           loader: "style-loader" // creates style nodes from JS strings
-      }, {
+        }, {
           loader: "css-loader" // translates CSS into CommonJS
-      }, {
+        }, {
           loader: "less-loader" // compiles Less to CSS
-      }]
+        }]
+      },
+
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              //当加载的图片，小于limit时，会将字符图片编译成base64字符串形式  
+              //当加载的图片，大于limit时，需要使用file-loader模块进行加载
+              limit: 10000,
+              name: 'img/[name].[hash:8].[ext]'
+            },
+          }
+        ]
       }
     ]
   }
