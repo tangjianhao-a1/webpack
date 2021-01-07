8c596a2ed8a40786fe442cac42e002bee6e15861
ede <837221025@qq.com>
2021-01-07 23:00:01 +0800

    基本完成，

--- 04.webpack-plugin/webpack.config.js
+++ 04.webpack-plugin/webpack.config.js
@@ -89,8 +89,10 @@ module.exports = {
     new UglifyjsWebpackPlugin()
   ],
   devServer: {
-    contentBase: './dist',  
-    inline: true,
+    contentBase: './dist',  //服务文件夹
+    inline: true,    //是否需要随时监听   
+    port: 4567,
+
   }
 
 }
