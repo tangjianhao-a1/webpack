34ef45bdc64c1ec2505ad05ffe0981974ed2e76c
ede <837221025@qq.com>
2021-01-08 18:08:48 +0800

    缓存文件

---
+++ 05.webpack配置分离/src/vue/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <h2 class="childer">{{message}}</h2>  
+  <button>按钮</button>  
+  <h2>{{name}}</h2>  
+  <Cpn></Cpn>  
+  <div>
+    <Input/>
+  </div>
+  </div>
+</template>
+
+<script>
+import Cpn from './Cpn'
+import Input from './Input'
+export default {  
+  components: {
+    Cpn,  
+    Input
+  },
+  data() {
+    return {
+      message: 'Hello Webpack',
+      name: 'coderwhy'
+    }
+  }
+}
+</script>
+
+<style>
+.childer {
+  color: green;
+}
+</style>
